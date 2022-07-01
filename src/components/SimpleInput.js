bd4eba81b5f35df0ad1cef704ab2807e28ac9447
Nataniel Mendes <nataniel.mendes@sfappworks.com>
2022-06-30 23:54:20 -0300

    React To Lost Focus

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -10,6 +10,15 @@ const SimpleInput = (props) => {
     setEnteredName(event.target.value)
   }
 
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false)
+      return
+    }
+  }
+
   const formSubmissionHandler = (event) => {
     event.preventDefault()
 
@@ -45,6 +54,7 @@ const SimpleInput = (props) => {
           type='text'
           id='name'
           onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
           value={enteredName}
         />
         {nameInputIsInvalid && <p className="error-text">Name must not be empty.</p>}
